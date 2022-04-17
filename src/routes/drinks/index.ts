a454bad1e2211dbf31667cd077c3de296afbe77b
iammrtndev <68447700+iammrtndev@users.noreply.github.com>
2022-04-17 22:05:59 +0200

    feat: Captain Morgan

--- src/routes/drinks/index.ts
+++ src/routes/drinks/index.ts
@@ -1,17 +1,74 @@
 import type { RequestHandler } from '@sveltejs/kit'
 
 export const get: RequestHandler = async (e) => {
-  const drinks = []
-  for (let i = 0; i < 10; i++) {
-    drinks.push(
-      {
-        id: i.toString(),
-        name: 'Hertog Jan',
-        description:
-          'Hertog Jan is a Dutch-Belgian beer that is brewed with a special yeast strain that produces a fruity, tart taste.',
-        image: '/hertog.jpg',
-      })
-  }
+  const drinks = [
+    {
+      id: 0,
+      name: 'Hertog Jan',
+      description:
+        'Hertog Jan is a Dutch-Belgian beer that is brewed with a special yeast strain that produces a fruity, tart taste.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 1,
+      name: 'Hertog Jan',
+      description:
+        'Hertog Jan is a Dutch-Belgian beer that is brewed with a special yeast strain that produces a fruity, tart taste.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 2,
+      name: 'Hertog Jan',
+      description:
+        'Hertog Jan is a Dutch-Belgian beer that is brewed with a special yeast strain that produces a fruity, tart taste.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 3,
+      name: 'Hertog Jan',
+      description:
+        'Hertog Jan is a Dutch-Belgian beer that is brewed with a special yeast strain that produces a fruity, tart taste.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 4,
+      name: 'Hertog Jan',
+      description:
+        'Hertog Jan is a Dutch-Belgian beer that is brewed with a special yeast strain that produces a fruity, tart taste.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 5,
+      name: 'Hertog Jan',
+      description:
+        'Hertog Jan is a Dutch-Belgian beer that is brewed with a special yeast strain that produces a fruity, tart taste.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 6,
+      name: 'Captain Morgan',
+      description: 'Captain Morgan is a strong, dark, and highly carbonated beer.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 7,
+      name: 'Captain Morgan',
+      description: 'Captain Morgan is a strong, dark, and highly carbonated beer.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 8,
+      name: 'Captain Morgan',
+      description: 'Captain Morgan is a strong, dark, and highly carbonated beer.',
+      image: '/hertog.jpg',
+    },
+    {
+      id: 9,
+      name: 'Captain Morgan',
+      description: 'Captain Morgan is a strong, dark, and highly carbonated beer.',
+      image: '/hertog.jpg',
+    },
+  ]
 
   return {
     status: 200,
